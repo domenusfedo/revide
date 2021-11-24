1f9a6f80953c0da3701ab4a6b555bc4860d3883d
domenusfedo <domanusfedo@gmail.com>
2021-11-24 16:55:01 +0100

    redux eventsSlice design

--- src/features/eventsSlice.ts
+++ src/features/eventsSlice.ts
@@ -13,7 +13,6 @@ export interface Event {
 }
 
 interface EventsState {
-    events: Event[],
     saved: Event[],
     dashboard: {
         highlightedEvent: Event,
@@ -34,7 +33,7 @@ const highlightedEvent: Event = {
     participants: 69,
 }
 
-//Pagination @FelsonInteligent Api should response with array of 5 elements: Event
+//Pagination @FelsonInteligent Api should response with array of 5 elements: Event based on actual page 1:[1E, 2E, 3E, 4E, 5E] 2:[6E, 7E, 8E, 9E, 10E]
 const dashboardEvents: Event[] = [
     {
         createdAt: new Date(),
@@ -94,26 +93,15 @@ const dashboardEvents: Event[] = [
 export const fetchFiveEvents = createAsyncThunk(
     'fetch/allEvents',
     async () => {
-        //fetch all events data
+        //fetch 5 elements: Event based on actual page 1:[1E, 2E, 3E, 4E, 5E]
+        //replace in state / or add to chunks 1:[1E, 2E, 3E, 4E, 5E] 2:[6E, 7E, 8E, 9E, 10E]
+        //if fetched element === {}; fetched element = {title: 'It can be Your event' <- incentive to create an Event <- when clikced redirect to Add new Event}
         //part of state.events
     }
 )
 
 const initialState: EventsState = {
-    events: [
-        {
-            createdAt: new Date(),
-            editedAt: new Date(),
-            location: 'Warsaw',
-            title: 'Fruit Party',
-            author: {
-                id: '1231235672',
-                username: 'Testnazwy'
-            },
-            participants: 69,
-        }
-    ],
-    saved: [],
+    saved: [], //User's created/saved events visible in Profile section
     dashboard: {
         highlightedEvent,
         dashboardEvents //This should be always array of 5 elements
